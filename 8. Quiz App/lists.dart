b71858dd85dc345f5916c6529b0ad9089e8aab40
Forest Park <foresthpark@gmail.com>
2020-04-10 11:29:47 -0600

    test

---
+++ 8. Quiz App/lists.dart
@@ -0,0 +1,13 @@
+void main() {
+    List<String> myList = ['Forest', 'Dustin', "Ann", "Nikola", "James"];
+
+    print(myList[2]);
+
+    print(myList.indexOf("Forest"));
+
+    myList.add("Leon");
+
+    myList.insert(3, 'Veronica');
+
+    print(myList);
+}
